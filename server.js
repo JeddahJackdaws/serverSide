cbbb033d4c0c939c5431c61d7fb3442078f819c5
Bassam <32364350+Bxssam@users.noreply.github.com>
2018-04-08 06:21:14 +0300

    fix the api
    add spec search for the doctor

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
-const jdb = require('./serverHandler')
-
+var port = process.env.PORT || 3000;
 const url = 'mongodb://bes:root@ds249128.mlab.com:49128/jackdaws';
 const dbName = 'jackdaws'
-
+const collectionName1 = 'D';
+const collectionName2 = 'H';
 var db
 
 MongoClient.connect(url, (err, client) => {
     if (err) return console.log(err)
     db = client.db(dbName)
-    app.listen(3000, () => {
-        console.log('listening on 3000')
+    app.listen(port, () => {
+        console.log('listening on ' + port)
     })
 })
 
 app.get('/doctors', function(req, res) {
     console.log(req.query)
-    jdb.getDoc(db, req.query, function(err, results) {
+    getDoc(db, req.query, function(err, results) {
         if (err) {
             res.status(500).send({ error: 'Oops something failed!' })
         }
@@ -28,7 +28,17 @@ app.get('/doctors', function(req, res) {
 
 app.get('/doctors/name/:name', function(req, res) {
     console.log(req.params.name)
-    jdb.getDocN(db, req.params.name, function(err, results) {
+    getDocN(db, req.params.name, function(err, results) {
+        if (err) {
+            res.status(500).send({ error: 'Oops something failed!' })
+        }
+        res.send(results)
+    })
+})
+
+app.get('/doctors/spec/:speciality', function(req, res) {
+    console.log(req.params.speciality)
+    getDocS(db, req.params.speciality, function(err, results) {
         if (err) {
             res.status(500).send({ error: 'Oops something failed!' })
         }
@@ -38,7 +48,7 @@ app.get('/doctors/name/:name', function(req, res) {
 
 app.get('/hospitals', function(req, res) {
     console.log(req.query)
-    jdb.getH(db, req.query, function(err, results) {
+    getH(db, req.query, function(err, results) {
         if (err) {
             res.status(500).send({ error: 'Oops something failed!' })
         }
@@ -48,10 +58,47 @@ app.get('/hospitals', function(req, res) {
 
 app.get('/hospitals/name/:name', function(req, res) {
     console.log(req.params.name)
-    jdb.getHN(db, req.params.name, function(err, results) {
+    getHN(db, req.params.name, function(err, results) {
         if (err) {
             res.status(500).send({ error: 'Oops something failed!' })
         }
         res.send(results)
     })
 })
+
+
+
+function getDoc(db, searchTerm, callback) {
+    db.collection(collectionName1).find(searchTerm).toArray(function(err, docs) {
+        if (err) { callback(err, null) }
+        callback(null, docs);
+    })
+}
+
+function getDocN(db, searchTerm, callback) {
+    db.collection(collectionName1).find({ name: new RegExp(searchTerm) }).toArray(function(err, docs) {
+        if (err) { callback(err, null) }
+        callback(null, docs);
+    })
+}
+
+function getDocS(db, searchTerm, callback) {
+    db.collection(collectionName1).find({ speciality: new RegExp(searchTerm) }).toArray(function(err, docs) {
+        if (err) { callback(err, null) }
+        callback(null, docs);
+    })
+}
+
+function getH(db, searchTerm, callback) {
+    db.collection(collectionName2).find(searchTerm).toArray(function(err, docs) {
+        if (err) { callback(err, null) }
+        callback(null, docs);
+    })
+}
+
+function getHN(db, searchTerm, callback) {
+    db.collection(collectionName2).find({ name: new RegExp(searchTerm) }).toArray(function(err, docs) {
+        if (err) { callback(err, null) }
+        callback(null, docs);
+    })
+}
